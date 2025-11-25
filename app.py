406de6bccf298ecdb178ecfbd9d79e9631bb3ad0
amitsternishai <amitsternishai@gmail.com>
2025-11-25 11:28:14 +0200

    uploading files to s3

--- app.py
+++ app.py
@@ -3,6 +3,16 @@ import threading
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 
+from flask import Flask, request, jsonify
+import boto3
+from botocore.exceptions import ClientError
+
+# app = Flask(__name__)
+
+# Your S3 settings
+S3_BUCKET = "amit-ishai-rag-bucket-2"
+s3 = boto3.client("s3")
+
 # ---------------------------
 # Config
 # ---------------------------
@@ -36,7 +46,29 @@ def health():
 
 @app.post("/upload")
 def upload():
-    return jsonify({"saved": "saved bla bla"})
+    if "file" not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files["file"]
+    if file.filename == "":
+        return jsonify({"error": "No selected file"}), 400
+
+    key = f"{file.filename}"
+
+    try:
+        s3.upload_fileobj(
+            Fileobj=file,
+            Bucket=S3_BUCKET,
+            Key=key,
+            ExtraArgs={"ContentType": file.content_type}
+        )
+    except ClientError as e:
+        return jsonify({"error": str(e)}), 500
+
+    return jsonify({
+        "message": "File uploaded successfully",
+        "s3_key": key
+    })
 
 @app.post("/reindex")
 def reindex():
@@ -50,4 +82,4 @@ def ask():
 # Start
 # ---------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8009)), debug=True)
